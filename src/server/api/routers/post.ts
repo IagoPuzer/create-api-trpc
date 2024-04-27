465fa41d15f90e9bde7d180ecbc7beef0d9a5c99
iagop <iagopuzer06@hotmail.com>
2024-04-27 20:20:58 -0300

    first commit

---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const postRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  create: publicProcedure
+    .input(
+      z.object({ name: z.string().min(1), description: z.string().min(1) }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      return ctx.db.post.create({
+        data: {
+          name: input.name,
+          description: input.description,
+        },
+      });
+    }),
+
+  getLatest: publicProcedure.query(({ ctx }) => {
+    return ctx.db.post.findFirst({
+      orderBy: { createdAt: "desc" },
+    });
+  }),
+
+  listPosts: publicProcedure.query(({ ctx }) => {
+    return ctx.db.post.findMany();
+  }),
+});
