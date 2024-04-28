95a2e83f591877214770e5fc28e6233c2bba7141
iagop <iagopuzer06@hotmail.com>
2024-04-28 14:25:31 -0300

    colocando validação de erros no createPost

--- src/app/_components/CreatePost.tsx
+++ src/app/_components/CreatePost.tsx
@@ -9,6 +9,7 @@ export function CreatePost() {
   const router = useRouter();
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [error, setError] = useState<string>("");
 
   const createPost = api.post.create.useMutation({
     onSuccess: () => {
@@ -18,14 +19,19 @@ export function CreatePost() {
     },
   });
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createPost.mutate({ name, description });
+    if (!name.trim() || !description.trim()) {
+      setError("Os campos de título e/ou descrição não podem estar vazios.");
+      return;
+    } else {
+      setError("");
+    }
+  };
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        createPost.mutate({ name, description });
-      }}
-      className="flex flex-col gap-2"
-    >
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2">
       <input
         type="text"
         placeholder="Título"
@@ -39,6 +45,7 @@ export function CreatePost() {
         onChange={(e) => setDescription(e.target.value)}
         className="mb-2 block h-40 w-full rounded border border-gray-300 bg-white p-2 text-black focus:outline-none"
       />
+      {error && <p className="p-4 text-red-500">{error}</p>}
       <button
         type="submit"
         className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
