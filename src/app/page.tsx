8daf12ef552b98c1615804a29f17d345f7edea8f
iagop <iagopuzer06@hotmail.com>
2024-04-28 00:17:29 -0300

    Alterações na Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,31 @@
 import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/server";
+import NoteCard from "./_components/card/NoteCard";
 
 export default async function Home() {
-  const getAllPost = await api.post.listPosts();
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <CrudShowcase />
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-          {getAllPost.map((post) => (
-            <div key={post.id} className="flex flex-col gap-2">
-              <h2 className="text-xl font-bold">{post.name}</h2>
-              <p>{post.description}</p>
-            </div>
-          ))}
+        <div className="w-full max-w-xs">
+          <h1 className="mb-6 flex w-full justify-center text-3xl">
+            Notes APP
+          </h1>
+          <CreatePost />
         </div>
+        <ShowPosts />
       </div>
     </main>
   );
 }
 
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest();
+async function ShowPosts() {
+  const getAllPost = await api.post.listPosts();
 
   return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">
-          Your most recent post: {latestPost.name} {latestPost.description}
-        </p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+      {getAllPost.map((post) => (
+        <NoteCard key={post.id} post={post} />
+      ))}
     </div>
   );
 }
